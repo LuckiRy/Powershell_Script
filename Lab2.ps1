f82945e8c4048aedd832ec8e84aa92c2a159143c
LuckiRy <ryanbrowntt@yahoo.com>
2024-05-30 15:46:46 -0700

    fourth

--- Lab2.ps1
+++ Lab2.ps1
@@ -1,14 +1,26 @@
-New-Aduser "Ryan B." -UserPrinicipalName Shell@testdomain.local -Givename Ryan -Surname Brown
-set-ADAccountPassword -Identity "Ryan B."
+New-Aduser 'Ryan B.' -UserPrincipalName Ryry@Shell.local -Givename Ryan -Surname Brown
+set-ADAccountPassword -Identity 'Ryan B.'
 
-enable-ADAccount -Identity "Ryan B."
-Get-ADUser "Ryan B."
+enable-ADAccount -Identity 'Ryan B.'
+Get-ADUser 'Ryan B.'
 
-$domain ="ShellDomian.local"
+$domain ="Shell.local"
 $user = "Administrator"
 $password=Read-Host -prompt "Enter your password" -AsSecureString
 
 $username ="domain\$user"
 $credential =New-Object System.Management .Automation.PSCredential($username,$password)
 
-Add-Computer -DomainName $domain -Credential $credential
+Add-Computer -DomainName $Shell -Credential $credential
+
+Rename-Computer -ComputerName "WIN-P08VF4E01F2" -NewName "Sever01"
+
+New-LocalGroup -name "Adminclu" -Description "Admin Dept"
+New-LocalGroup -name "RyansAdminclu" -Description "Admin Dept"
+New-LocalGroup -name "RyansAdmin" -Description "Admin Dept"
+
+Remove-ADGroup -Identity RyansAdmin
+
+Restart-Computer
+
+
